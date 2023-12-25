2c18053350147ed1baad063263c8d34bda4166d9
dooney <delzeen55@gmail.com>
2023-12-25 22:08:24 +0100

    initialized dashboard page

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+const Dashboard = () => {
+  return (
+    <div>Dashboard <Link to='/register'>Register</Link></div>
+  )
+}
+
+export default Dashboard
