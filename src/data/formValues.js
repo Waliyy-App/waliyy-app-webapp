80d05fa29ac557742175c50d52fd8a349fe2d736
dooney <delzeen55@gmail.com>
2024-01-01 22:42:33 +0100

    added form values

---
+++ src/data/formValues.js
@@ -0,0 +1,20 @@
+export const heightRanges = [
+  { label: 'Below 4\'11"', value: 'below_150_cm' },
+  { label: '5\'0" - 5\'3"', value: '150_160_cm' },
+  { label: '5\'4" - 5\'7"', value: '160_170_cm' },
+  { label: '5\'8" - 5\'11"', value: '170_180_cm' },
+  { label: 'Above 6\'0"', value: 'above_180_cm' },
+];
+
+export const weightRanges = [
+  { label: 'Below 50 kg or Below 110 lbs', value: 'below_50_kg' },
+  { label: '50 - 60 kg or 110 - 132 lbs', value: '50_60_kg' },
+  { label: '60 - 70 kg or 132 - 154 lbs', value: '60_70_kg' },
+  { label: '70 - 80 kg or 154 - 176 lbs', value: '70_80_kg' },
+  { label: '80 - 90 kg or 176 - 198 lbs', value: '80_90_kg' },
+  { label: '90 - 100 kg or 198 - 220 lbs', value: '90_100_kg' },
+  { label: '100 - 110 kg or 220 - 242 lbs', value: '100_110_kg' },
+  { label: '110 - 120 kg or 242 - 264 lbs', value: '110_120_kg' },
+  { label: '120 - 130 kg or 264 - 286 lbs', value: '120_130_kg' },
+  { label: 'Above 130 kg or Above 286 lbs', value: 'above_130_kg' },
+];
