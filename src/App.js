b8f95d86fa2d9d55324d0488bcbe31e3ce8b543c
dooney <delzeen55@gmail.com>
2024-01-06 08:50:45 +0100

    added routes to recently created pages

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import ProfileSetup from './pages/ProfileSetup';
+import LikePage from './pages/LikePage';
+import MatchPage from './pages/MatchPage';
+import SettingsPage from './pages/SettingsPage';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/likes" element={<LikePage />} />
+        <Route path="/match" element={<MatchPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+
+        <Route path="/sign-up" element={<Register />} />
         <Route path="/sign-in" element={<Login />} />
-        <Route path="/setup" element={<ProfileSetup />} />
+        <Route path="/sign-up/setup" element={<ProfileSetup />} />
       </Routes>
     </div>
   );
