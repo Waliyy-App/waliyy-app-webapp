64e9f38753c083e6aed66e6a2a37fd1e4f8cbb7a
dooney <delzeen55@gmail.com>
2024-01-07 22:28:38 +0100

    added profile card to dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import SidebarComponent from '../components/sidebar/Sidebar';
+import ProfileView from '../components/ProfileCard';
+import { HiOutlineAdjustmentsHorizontal } from "react-icons/hi2";
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
   return (
-    <div>
+    <div className="flex">
       <SidebarComponent />
+      <main className="ml-[280px] py-[64px] px-8 w-full">
+        <Link to='/filter' className='flex justify-end py-8'>
+          <HiOutlineAdjustmentsHorizontal className='h-8 w-8' />
+        </Link>
+        <div className="flex flex-wrap gap-6">
+          <ProfileView />
+          <ProfileView />
+          <ProfileView />
+          <ProfileView />
+          <ProfileView />
+          <ProfileView />
+          <ProfileView />
+        </div>
+      </main>
     </div>
   );
 };
