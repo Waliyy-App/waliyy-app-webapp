5eab0fdacac4f153a45433d2c990f22168d9c327
dooney <delzeen55@gmail.com>
2024-01-09 00:29:18 +0100

    added function for profile

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import Logo from '../../assets/logo/logo-nobg.png';
 import Female from '../../assets/illustrations/female-illus.png';
-import { Link, NavLink, useLocation } from 'react-router-dom';
-import {
-  FiHeart,
-  FiHome,
-  FiLogOut,
-  FiSettings,
-  FiThumbsUp,
-} from 'react-icons/fi';
+import { Link, NavLink, useLocation, useNavigate } from 'react-router-dom';
+import ThumbUpIcon from '@mui/icons-material/ThumbUpAlt';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import HomeIcon from '@mui/icons-material/Home';
+import SettingsIcon from '@mui/icons-material/Settings';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const SidebarComponent = () => {
   const location = useLocation();
 
+  const navigate = useNavigate();
+
+ const handleProfileClick = () => {
+    // Assuming isUser is a boolean variable
+    const isUser = true;
+
+    // Navigate to the profile page with the state
+    navigate('/profile', { state: { isUser } });
+  };
   return (
     <div className="h-screen flex">
       <aside className="w-[280px] bg-[#F2EEFB] px-8 pb-8 flex flex-col justify-between fixed inset-y-0 left-0 h-screen">
@@ -34,7 +41,7 @@ const SidebarComponent = () => {
                   : 'text-[#2D133A]'
               }`}
             >
-              <FiHome className="h-6 w-6" /> Home
+              <HomeIcon /> Home
             </NavLink>
 
             <NavLink
@@ -45,7 +52,7 @@ const SidebarComponent = () => {
                   : 'text-[#2D133A]'
               }`}
             >
-              <FiThumbsUp className="h-6 w-6" /> Likes
+              <ThumbUpIcon /> Likes
             </NavLink>
 
             <NavLink
@@ -56,7 +63,7 @@ const SidebarComponent = () => {
                   : 'text-[#2D133A]'
               }`}
             >
-              <FiHeart className="h-6 w-6" /> Match
+              <FavoriteIcon /> Match
             </NavLink>
 
             <NavLink
@@ -67,22 +74,23 @@ const SidebarComponent = () => {
                   : 'text-[#2D133A]'
               }`}
             >
-              <FiSettings className="h-6 w-6" /> Settings
+              <SettingsIcon /> Settings
             </NavLink>
           </div>
         </div>
         <div className="flex flex-col items-end gap-6">
           <div className="border border-[#2D133A] w-full"></div>
           <div className="flex items-center text-[#2D133A] w-full justify-between px-2">
-            <Link
-              to="/"
-              className="flex gap-2 items-center text-sm font-semibold"
+            <div
+              onClick={handleProfileClick}
+              className="flex gap-2 items-center text-sm font-semibold cursor-pointer"
             >
               <img src={Female} alt="" className="h-8 w-8" />
               Raufah
-            </Link>
-            <Link to="/" className=" h-4 w-4">
-              <FiLogOut />
+            </div>
+
+            <Link to="/">
+              <LogoutIcon />
             </Link>
           </div>
         </div>
