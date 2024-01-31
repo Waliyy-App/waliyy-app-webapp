ce29e4e23519116c745ec5b0fc2939f549932b82
dooney <delzeen55@gmail.com>
2024-01-31 13:18:09 +0100

    deleted unused code

--- src/components/setupForms/NationalityForm.js
+++ src/components/setupForms/NationalityForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Formik, Form } from 'formik';
 import { TextInput, SelectInput } from '../../common/form';
 import {
