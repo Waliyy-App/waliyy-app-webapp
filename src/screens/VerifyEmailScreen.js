588aa9b3bb9852fc3fcbf47ab6bc11f2b8d620c8
dooney <delzeen55@gmail.com>
2024-02-17 03:02:47 +0100

    added email verification screen

---
+++ src/screens/VerifyEmailScreen.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import ClapLogo from '../assets/illustrations/clap.png';
+
+const VerifyEmailScreen = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      navigate('/');
+    }, 5000);
+    return () => clearTimeout(timeoutId);
+  });
+  
+  return (
+    <div className="w-100 h-screen bg-white flex flex-col items-center justify-center">
+      <div className="w-[360px] mx-auto ">
+        <img src={ClapLogo} alt="." className="w-[200px] h-[200px] mx-auto" />
+        <div className="flex flex-col items-center jutify-center mt-8">
+          <p className="text-2xl text-[#2D133A] font-medium mb-3">
+            Congratulations!!!
+          </p>
+          <p className="text-[#665e6b] text-center font-normal">
+            Your account is all set!
+            <br />
+            <br />
+            To get started, verify your email address by clicking the link in
+            the email we've sent you.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VerifyEmailScreen;
