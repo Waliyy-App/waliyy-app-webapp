c2058c25adec1776b269dc6b99f6aaa2bdb40508
dooney <delzeen55@gmail.com>
2024-02-17 03:02:32 +0100

    added forgot password link

--- src/App.js
+++ src/App.js
@@ -11,19 +11,19 @@ import PricingPage from './pages/PricingPage';
 import { Filters } from './pages/Filters';
 import ProfileDetails from './components/profile/ProfileDetails';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
-
-const theme = createTheme({
-  typography: {
-    fontFamily: 'Nunito, san-serif',
-  },
-   palette: {
-    secondary: {
-      main: '#BA9FFE',
+  const theme = createTheme({
+    typography: {
+      fontFamily: 'Nunito, san-serif',
     },
-  },
-});
+    palette: {
+      secondary: {
+        main: '#BA9FFE',
+      },
+    },
+  });
 
   return (
     <ThemeProvider theme={theme}>
@@ -34,11 +34,10 @@ const theme = createTheme({
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/pricing" element={<PricingPage />} />
 
-
         <Route path="/sign-up" element={<Register />} />
         <Route path="/" element={<Login />} />
         <Route path="/sign-up/setup" element={<ProfileSetup />} />
-
+        <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/filter" element={<Filters />} />
 
         <Route path="/profile" element={<ProfileDetails />} />
