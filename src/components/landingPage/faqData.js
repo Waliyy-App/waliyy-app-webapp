7d428a2686318acc447ca8e926bc49add6017381
dooney <delzeen55@gmail.com>
2024-03-20 10:37:08 +0100

    faq section done

---
+++ src/components/landingPage/faqData.js
@@ -0,0 +1,42 @@
+export const faq = [
+  {
+    id: 1,
+    question: "What is WaliyyApp?",
+    answer: "WaliyyApp is a marriage platform where guardians are given priority in supporting their wards find their ever after spouses."
+  },
+  {
+    id: 2,
+    question: "How do I join the platform?",
+    answer: "Click on the sign up button!."
+  },
+  {
+    id: 3,
+    question: "Can I register for more than one child/ward?",
+    answer: "Of course! We at WaliyyApp understand that guardians have multiple wards who need to get married."
+  },
+  {
+    id: 4,
+    question: "Is my data safe?",
+    answer: "Yes! WaliyyApp uses security measures to ensure the safety of your data."
+  },
+  {
+    id: 5,
+    question: "What currencies are accepted for payments?",
+    answer: "WaliyyApp accepts payments in £, $, and Naira."
+  },
+  {
+    id: 6,
+    question: "Will I get notification when someone likes my profile?",
+    answer: "Yes, you will receive an email notification when someone likes your profile."
+  },
+  {
+    id: 7,
+    question: "Do I need to upload my photo?",
+    answer: "WaliyyApp protects your privacy, there is no feature to upload photos."
+  },
+  {
+    id: 8,
+    question: "Can I chat with someone I like on the platform?",
+    answer: "WaliyyApp protects the interests of sisters; as such, WaliyyApp should make contact in the first instance and set up supervised meetings for their wards."
+  }
+];
