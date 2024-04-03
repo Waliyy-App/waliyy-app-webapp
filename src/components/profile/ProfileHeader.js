4dcdbb12205d1defa076c51424ecf8c2a5585d6b
dooney <delzeen55@gmail.com>
2024-04-03 09:13:10 +0100

    added a check for the like button

--- src/components/profile/ProfileHeader.js
+++ src/components/profile/ProfileHeader.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import MaleIcon from '../../assets/illustrations/male-illus.svg';
-import FemaleIcon from '../../assets/illustrations/female-illus.svg';
+// import FemaleIcon from '../../assets/illustrations/female-illus.svg'; for if gender === 'FEMALE'
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import { BsFillDiamondFill } from 'react-icons/bs';
 import ThumbUpIcon from '@mui/icons-material/ThumbUpAlt';
-import ThumbDownIcon from '@mui/icons-material/ThumbDownAlt';
+import { useAuthContext } from '../../context/AuthContext';
+const ProfileHeader = () => {
+  const { user } = useAuthContext();
 
-const ProfileHeader = ({ isUser, data }) => {
   return (
     <div className="flex flex-col sm:flex-row items-center sm:items-end justify-between py-8 gap-10">
       <div className="flex flex-col sm:flex-row sm:justify-start items-center gap-4">
         <div className="flex items-center justify-center h-[155px] w-[155px] rounded-full border border-[#0000000d] bg-white box-shadow-profile z-30 relative overflow-hidden">
-          {isUser.gender === 'FEMALE' ? (
-            <img src={FemaleIcon} alt='' className="w-24 h-24 z-40" />
-          ) : (
-            <img src={MaleIcon} alt='' className="w-24 h-24 z-40" />
-          )}
+          <img src={MaleIcon} alt="" className="w-24 h-24 z-40" />
         </div>
 
         <div className="text-[#2D133A] text-center sm:text-left">
@@ -33,13 +30,10 @@ const ProfileHeader = ({ isUser, data }) => {
         </div>
       </div>
 
-      {!isUser && (
+      {!user && (
         <div className="flex items-center gap-3 self-center sm:self-end">
           <button className="hover:bg-[#a37eff] bg-[#BA9FFE] rounded-lg h-11 text-white font-medium box-shadow-style px-5 flex items-center gap-2 transition-all duration-300">
-            <ThumbUpIcon /> Like
-          </button>
-          <button className="bg-[#2D133A] hover:bg-[#14091a] rounded-lg h-11 text-white font-medium box-shadow-style px-5 flex items-center gap-2 transition-all duration-300">
-            Pass <ThumbDownIcon />
+            <ThumbUpIcon /> Interested
           </button>
         </div>
       )}
