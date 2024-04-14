24d17fe60d6744136028ad859b0c698641ef4559
dooney <delzeen55@gmail.com>
2024-04-14 20:34:50 +0100

    added capitalize function

--- src/components/profile/DeenProfile.js
+++ src/components/profile/DeenProfile.js
@@ -1,71 +1,66 @@
-import React from "react";
-import CustomTabPanel from "../../common/CustomTabPanel";
+import React from 'react';
+import CustomTabPanel from '../../common/CustomTabPanel';
+import { capitalize } from '../../utils.js';
 
 const DeenProfile = ({
-	value,
-	practiceDesc,
-	speakers,
-	revert,
-	salatPattern,
-	sect,
-	startedPracticingIn,
+  value,
+  practiceDesc,
+  speakers,
+  revert,
+  salatPattern,
+  sect,
+  startedPracticingIn,
 }) => {
-	return (
-		<CustomTabPanel value={value} index={2}>
-			<div className="flex flex-col items-start gap-10 py-8">
-				<div className="flex flex-col gap-6 text-[#2D133A] w-full sm:w-4/5">
-					<div>
-						<p className="mb-[10px] text-lg font-semibold">
-							About my Islamic Practices!
-						</p>
-						<p>
-							{practiceDesc ||
-								`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-							enim ad minim veniam, quis nostrud exercitation ullamco laboris
-							nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-							reprehenderit in voluptate velit esse cillum dolore eu fugiat
-							nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-							sunt in culpa qui officia deserunt mollit anim id est laborum.`}
-						</p>
-					</div>
+  return (
+    <CustomTabPanel value={value} index={2}>
+      <div className="flex flex-col items-start gap-10 py-8">
+        <div className="flex flex-col gap-6 text-[#2D133A] w-full sm:w-4/5">
+          <div>
+            <p className="mb-[10px] text-lg font-semibold">
+              About my Islamic Practices!
+            </p>
+            <p>{practiceDesc}</p>
+          </div>
 
-					<div>
-						<p className="mb-[10px] text-lg font-semibold">
-							Speakers and Scholars I listen to
-						</p>
-						<div className="flex gap-3">
-							{speakers?.map((speaker) => <p key={speaker}>{speaker}</p>) ||
-								"Yasir Qahdi"}
-						</div>
-					</div>
-				</div>
+          {speakers ? (
+            <div>
+              <p className="mb-[10px] text-lg font-semibold">
+                Speakers and Scholars I listen to
+              </p>
+              <div className="flex gap-3">
+                {speakers.map((speaker) => (
+                  <p key={speaker}>{speaker}</p>
+                ))}
+              </div>
+            </div>
+          ) : ''}
+        </div>
 
-				<div className="w-full sm:w-2/5">
-					<p className="text-lg font-semibold mb-4">My Details</p>
-					<div className="flex flex-wrap gap-4">
-						<div className="p-2 rounded-2xl bg-[#FFF4F5] text-[#2D133A] text-xs font-bold">
-							{sect || "Sunni"}
-						</div>
+        <div className="w-full sm:w-2/5">
+          <p className="text-lg font-semibold mb-4">My Details</p>
+          <div className="flex flex-wrap gap-4">
+            <div className="p-2 rounded-2xl bg-[#FFF4F5] text-[#2D133A] text-xs font-bold">
+              {capitalize(sect)}
+            </div>
 
-						{revert && (
-							<div className="p-2 rounded-2xl bg-[#FFF4F5] text-[#2D133A] text-xs font-bold">
-								Revert
-							</div>
-						)}
+            {revert && (
+              <div className="p-2 rounded-2xl bg-[#FFF4F5] text-[#2D133A] text-xs font-bold">
+                Revert
+              </div>
+            )}
 
-						<div className="p-2 rounded-2xl bg-[#FFF4F5] text-[#2D133A] text-xs font-bold">
-							{salatPattern || "Pray 5 times daily"}
-						</div>
+            <div className="p-2 rounded-2xl bg-[#FFF4F5] text-[#2D133A] text-xs font-bold">
+              {salatPattern}
+            </div>
 
-						<div className="p-2 rounded-2xl bg-[#FFF4F5] text-[#2D133A] text-xs font-bold">
-							{startedPracticingIn || "Started practicing recently"}
-						</div>
-					</div>
-				</div>
-			</div>
-		</CustomTabPanel>
-	);
+            <div className="p-2 rounded-2xl bg-[#FFF4F5] text-[#2D133A] text-xs font-bold">
+              {capitalize(startedPracticingIn)}
+            </div>
+          </div>
+        </div>
+      </div>
+    </CustomTabPanel>
+  );
 };
 
 export default DeenProfile;
--- src/components/profile/EduProfile.js
+++ src/components/profile/EduProfile.js
@@ -1,101 +1,87 @@
-import React from "react";
-import CustomTabPanel from "../../common/CustomTabPanel";
-import SchoolIcon from "@mui/icons-material/School";
-import WorkIcon from "@mui/icons-material/Work";
+import React from 'react';
+import CustomTabPanel from '../../common/CustomTabPanel';
+import SchoolIcon from '@mui/icons-material/School';
+import WorkIcon from '@mui/icons-material/Work';
 // import WorkOffIcon from '@mui/icons-material/WorkOff'; for if employmentStatus is unemployed
-import BadgeIcon from "@mui/icons-material/Badge";
+import BadgeIcon from '@mui/icons-material/Badge';
 import {
-	employmentStatusOptions,
-	educationOptions,
-} from "../../data/formValues";
+  employmentStatusOptions,
+  educationOptions,
+} from '../../data/formValues';
 
 const EduProfile = ({
-	value,
-	eduProf,
-	plans,
-	educationLevel,
-	employmentStatus,
-	profession,
-	relocationPlans,
-	isWillingToRelocate,
+  value,
+  eduProf,
+  plans,
+  educationLevel,
+  employmentStatus,
+  profession,
+  relocationPlans,
+  isWillingToRelocate,
 }) => {
-	const getEmploymentStatus = (value) => {
-		const employment = employmentStatusOptions?.filter(
-			(options) => options?.value === value
-		);
-		return employment?.[0]?.label;
-	};
-	const getEducationStatus = (value) => {
-		const education = educationOptions?.filter(
-			(options) => options?.value === value
-		);
-		return education?.[0]?.label;
-	};
-	return (
-		<CustomTabPanel value={value} index={1}>
-			<div className="flex flex-col items-start gap-10 py-8">
-				<div className="flex flex-col gap-6 text-[#2D133A] w-full sm:w-4/5">
-					<div>
-						<p className="mb-[10px] text-lg font-semibold">
-							About my education and profession!
-						</p>
-						<p>
-							{eduProf ||
-								`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-							enim ad minim veniam, quis nostrud exercitation ullamco laboris
-							nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-							reprehenderit in voluptate velit esse cillum dolore eu fugiat
-							nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-							sunt in culpa qui officia deserunt mollit anim id est laborum.`}
-						</p>
-					</div>
-					<div>
-						<p className="mb-[10px] text-lg font-semibold">
-							My short/medium term qualification and professional plans
-						</p>
-						<p>
-							{plans ||
-								`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-							enim ad minim veniam, quis nostrud exercitation ullamco laboris
-							nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-							reprehenderit in voluptate velit esse cillum dolore eu fugiat
-							nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-							sunt in culpa qui officia deserunt mollit anim id est laborum.`}
-						</p>
-					</div>
+  const getEmploymentStatus = (value) => {
+    const employment = employmentStatusOptions?.filter(
+      (options) => options?.value === value
+    );
+    return employment?.[0]?.label;
+  };
+  const getEducationStatus = (value) => {
+    const education = educationOptions?.filter(
+      (options) => options?.value === value
+    );
+    return education?.[0]?.label;
+  };
+  return (
+    <CustomTabPanel value={value} index={1}>
+      <div className="flex flex-col items-start gap-10 py-8">
+        <div className="flex flex-col gap-6 text-[#2D133A] w-full sm:w-4/5">
+          <div>
+            <p className="mb-[10px] text-lg font-semibold">
+              About my education and profession!
+            </p>
+            <p>
+              {eduProf}
+            </p>
+          </div>
+          <div>
+            <p className="mb-[10px] text-lg font-semibold">
+              My short/medium term qualification and professional plans
+            </p>
+            <p>
+              {plans}
+            </p>
+          </div>
 
-					{isWillingToRelocate && (
-						<div>
-							<p className="mb-[10px] text-lg font-semibold">
-								Willing to relocate to...
-							</p>
-							<p>{relocationPlans || "Europe"}</p>
-						</div>
-					)}
-				</div>
+          {isWillingToRelocate && (
+            <div>
+              <p className="mb-[10px] text-lg font-semibold">
+                Willing to relocate to...
+              </p>
+              <p>{relocationPlans}</p>
+            </div>
+          )}
+        </div>
 
-				<div className="w-full sm:w-2/5">
-					<p className="text-lg font-semibold mb-4">My Details</p>
-					<div className="flex flex-wrap gap-4">
-						<div className="p-2 rounded-2xl bg-[#FFF4F5] text-[#2D133A] text-xs font-bold flex items-center gap-1">
-							<SchoolIcon />{" "}
-							{getEmploymentStatus(employmentStatus) || "Masters"}
-						</div>
+        <div className="w-full sm:w-2/5">
+          <p className="text-lg font-semibold mb-4">My Details</p>
+          <div className="flex flex-wrap gap-4">
+            <div className="p-2 rounded-2xl bg-[#FFF4F5] text-[#2D133A] text-xs font-bold flex items-center gap-1">
+              <SchoolIcon />
+              {getEmploymentStatus(employmentStatus)}
+            </div>
 
-						<div className="p-2 rounded-2xl bg-[#FFF4F5] text-[#2D133A] text-xs font-bold  flex items-center gap-1">
-							<WorkIcon /> {getEducationStatus(educationLevel) || "Employed"}
-						</div>
+            <div className="p-2 rounded-2xl bg-[#FFF4F5] text-[#2D133A] text-xs font-bold  flex items-center gap-1">
+              <WorkIcon /> {getEducationStatus(educationLevel)}
+            </div>
 
-						<div className="p-2 rounded-2xl bg-[#FFF4F5] text-[#2D133A] text-xs font-bold  flex items-center gap-1">
-							<BadgeIcon /> {profession || "Web developer"}
-						</div>
-					</div>
-				</div>
-			</div>
-		</CustomTabPanel>
-	);
+            <div className="p-2 rounded-2xl bg-[#FFF4F5] text-[#2D133A] text-xs font-bold  flex items-center gap-1">
+              <BadgeIcon /> {profession}
+            </div>
+          </div>
+        </div>
+      </div>
+    </CustomTabPanel>
+  );
 };
 
 export default EduProfile;
