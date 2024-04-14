ca857ae76ba3e62b4e6f4c4ec31d8eda2d8ee451
dooney <delzeen55@gmail.com>
2024-04-14 20:39:34 +0100

    formatted code

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,78 +1,78 @@
-import React, { useState, useEffect, useContext } from "react";
-import { useCookies } from "react-cookie";
+import React, { useState, useEffect, useContext } from 'react';
+import { useCookies } from 'react-cookie';
 // import { useNavigate } from 'react-router-dom';
 
 export const AuthContext = React.createContext({
-	isLoggedIn: false,
-	token: "",
-	storeAuthCookie: (data) => {},
-	logOut: () => {},
+  isLoggedIn: false,
+  token: '',
+  storeAuthCookie: (data) => {},
+  logOut: () => {},
 });
 
 export const AuthContextProvider = ({ children }) => {
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
-	const [user, setUser] = useState();
-	const [cookies, setCookie, removeCookie] = useCookies(["waliyy_user"]);
-	const [token, setToken] = useState(cookies.waliyy_user);
-	const [data, setData] = useState({});
-	const [childId, setChildId] = useState("");
-	// const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState();
+  const [cookies, setCookie, removeCookie] = useCookies(['waliyy_user']);
+  const [token, setToken] = useState(cookies.waliyy_user);
+  const [data, setData] = useState({});
+  const [childId, setChildId] = useState('');
+  // const navigate = useNavigate();
 
-	function handleChildId(id) {
-		localStorage.setItem("childId", id);
-		setChildId(id);
-	}
+  function handleChildId(id) {
+    localStorage.setItem('childId', id);
+    setChildId(id);
+  }
 
-	function storeAuthCookie(data) {
-		setCookie("waliyy_user", data.token, {
-			path: "/",
-			maxAge: 3600 * 24 * 30, // 30 days
-		});
-		localStorage.setItem("user", JSON.stringify(data.user));
-		setUser(data.user);
-		setData(data);
-		setIsLoggedIn(true);
-	}
+  function storeAuthCookie(data) {
+    setCookie('waliyy_user', data.token, {
+      path: '/',
+      maxAge: 3600 * 24 * 30, // 30 days
+    });
+    localStorage.setItem('user', JSON.stringify(data.user));
+    setUser(data.user);
+    setData(data);
+    setIsLoggedIn(true);
+  }
 
-	function logOut() {
-		removeCookie("waliyy_user", {
-			path: "/",
-			maxAge: 3600 * 24 * 30,
-		});
-		setUser();
-		localStorage.removeItem("user");
-		localStorage.removeItem("childId");
-		setIsLoggedIn(false);
-	}
+  function logOut() {
+    removeCookie('waliyy_user', {
+      path: '/',
+      maxAge: 3600 * 24 * 30,
+    });
+    setUser();
+    localStorage.removeItem('user');
+    localStorage.removeItem('childId');
+    setIsLoggedIn(false);
+  }
 
-	useEffect(() => {
-		if (cookies.waliyy_user) {
-			setToken(cookies.waliyy_user);
-			setIsLoggedIn(true);
+  useEffect(() => {
+    if (cookies.waliyy_user) {
+      setToken(cookies.waliyy_user);
+      setIsLoggedIn(true);
 
-			const userData = JSON.parse(localStorage.getItem("user"));
-			setUser(userData);
-		}
-	}, [cookies.waliyy_user]);
+      const userData = JSON.parse(localStorage.getItem('user'));
+      setUser(userData);
+    }
+  }, [cookies.waliyy_user]);
 
-	return (
-		<AuthContext.Provider
-			value={{
-				user,
-				isLoggedIn,
-				token,
-				storeAuthCookie,
-				logOut,
-				data,
-				childId,
-				handleChildId,
-			}}
-		>
-			{children}
-		</AuthContext.Provider>
-	);
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        isLoggedIn,
+        token,
+        storeAuthCookie,
+        logOut,
+        data,
+        childId,
+        handleChildId,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuthContext = () => {
-	return useContext(AuthContext);
+  return useContext(AuthContext);
 };
