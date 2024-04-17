53dbed670715f7ce15115a2a0b4190a3ee5b63ea
dooney <delzeen55@gmail.com>
2024-04-17 12:30:18 +0100

    formatted the page

--- src/pages/LikePage.js
+++ src/pages/LikePage.js
@@ -1,31 +1,31 @@
-import React from "react";
-import SidebarComponent from "../components/sidebar/Sidebar";
-import Liked from "../components/likes/Liked";
-import { usePersistedState } from "../utils.js";
-import MobileNav from "../components/sidebar/MobileBottomNav.js";
-import MobileTopNav from "../components/sidebar/MobileTopNav.js";
+import React from 'react';
+import SidebarComponent from '../components/sidebar/Sidebar';
+import Liked from '../components/likes/Liked';
+import { usePersistedState } from '../utils.js';
+import MobileNav from '../components/sidebar/MobileBottomNav.js';
+import MobileTopNav from '../components/sidebar/MobileTopNav.js';
 
 const LikePage = () => {
-	const [isOpen, setIsOpen] = usePersistedState("isOpen", false);
+  const [isOpen, setIsOpen] = usePersistedState('isOpen', false);
 
-	const toggleMenu = () => {
-		setIsOpen(!isOpen);
-	};
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-	return (
-		<div className="flex flex-col sm:flex-row">
-			<SidebarComponent isOpen={isOpen} toggleMenu={toggleMenu} />
-			<MobileTopNav />
-			<main
-				className={`${
-					isOpen ? "ml-0 sm:ml-[100px]" : "ml-0 sm:ml-[280px]"
-				} py-[64px] px-8 w-full transition-all duration-300`}
-			>
-				<Liked />
-			</main>
-			<MobileNav />
-		</div>
-	);
+  return (
+    <div className="flex flex-col sm:flex-row">
+      <SidebarComponent isOpen={isOpen} toggleMenu={toggleMenu} />
+      <MobileTopNav />
+      <main
+        className={`${
+          isOpen ? 'ml-0 sm:ml-[100px]' : 'ml-0 sm:ml-[280px]'
+        } py-[64px] px-8 w-full transition-all duration-300`}
+      >
+        <Liked />
+      </main>
+      <MobileNav />
+    </div>
+  );
 };
 
 export default LikePage;
