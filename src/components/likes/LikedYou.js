ae03230775a93af1af877b24d91fe31d3942eda0
dooney <delzeen55@gmail.com>
2024-04-29 08:50:42 +0100

    added 'liked you' and 'viewed' to the like page

---
+++ src/components/likes/LikedYou.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import CustomTabPanel from '../../common/CustomTabPanel';
+
+const LikedYou = ({ value }) => {
+ 
+  return (
+    <div>
+      <CustomTabPanel value={value} index={1}>
+        <div className="flex flex-col items-center justify-center gap-3 text-center px-0 sm:px-8 pt-8 pb-[64px]">
+          <p className="text-[#2D133A] font-bold text-4xl">Liked You</p>
+          <p className="text-[#667085] text-xl">
+            These are the people that want to match with you. View their profile
+            and like them back to start something beautiful.
+          </p>
+        </div>
+      </CustomTabPanel>
+    </div>
+  );
+};
+
+export default LikedYou;
--- src/pages/LikePage.js
+++ src/pages/LikePage.js
@@ -1,27 +1,71 @@
 import React from 'react';
 import SidebarComponent from '../components/sidebar/Sidebar';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import ThumbUpIcon from '@mui/icons-material/ThumbUpAlt';
+import MoodIcon from '@mui/icons-material/EmojiEmotions';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import Liked from '../components/likes/Liked';
-import { usePersistedState } from '../utils.js';
+import LikedYou from '../components/likes/LikedYou';
+import { usePersistedState, a11yProps } from '../utils.js';
 import MobileNav from '../components/sidebar/MobileBottomNav.js';
 import MobileTopNav from '../components/sidebar/MobileTopNav.js';
+import Viewed from '../components/likes/Viewed.js';
 
 const LikePage = () => {
+  const [value, setValue] = React.useState(0);
+
   const [isOpen, setIsOpen] = usePersistedState('isOpen', false);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <div className="flex flex-col sm:flex-row">
       <SidebarComponent isOpen={isOpen} toggleMenu={toggleMenu} />
-      <MobileTopNav />
+       <MobileTopNav />
       <main
         className={`${
           isOpen ? 'ml-0 sm:ml-[100px]' : 'ml-0 sm:ml-[280px]'
         } py-[64px] px-8 w-full transition-all duration-300`}
       >
-        <Liked />
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          textColor="inherit"
+          className='overflow-scroll'
+          indicatorColor="secondary"
+          aria-label="icon position tabs example"
+          centered
+        >
+          <Tab
+            icon={<ThumbUpIcon />}
+            iconPosition="start"
+            label="Liked"
+            {...a11yProps(0)}
+          />
+          <Tab
+            icon={<MoodIcon />}
+            iconPosition="start"
+            label="Liked You"
+            {...a11yProps(1)}
+          />
+          <Tab
+            icon={<RemoveRedEyeIcon />}
+            iconPosition="start"
+            label="Viewed You"
+            {...a11yProps(2)}
+          />
+        </Tabs>
+
+        <Liked value={value} />
+        <LikedYou value={value} />
+        <Viewed value={value} />
       </main>
       <MobileNav />
     </div>
