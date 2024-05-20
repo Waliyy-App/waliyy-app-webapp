2ad03a5744615da8435dae545dc233a3686af9b1
DrPrime01 <gabrielshoyombo2002@gmail.com>
2024-05-20 23:07:44 +0100

    feat: updated UI

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -12,12 +12,19 @@ import { getChild, getRecommedations } from "../services/index.js";
 import Loader from "../components/Loader.js";
 
 const Dashboard = () => {
+	const PAGE_NUMBER = 12;
+
 	const [isOpen, setIsOpen] = usePersistedState("isOpen", false);
 	const [child, setChild] = useState({});
 	const [loading, setLoading] = useState(false);
 	const [recommedations, setRecommendations] = useState([]);
+	const [currRecommendations, setCurrRecommendations] = useState([]);
+	const [endPage, setEndPage] = useState(PAGE_NUMBER);
+	const [pageStart, setPageStart] = useState(0);
 	const { token } = useAuthContext();
 	const childId = localStorage.getItem("childId");
+
+	const usersLength = recommedations?.length;
 
 	useEffect(() => {
 		const getSuitors = async () => {
@@ -34,6 +41,26 @@ const Dashboard = () => {
 
 		getSuitors();
 	}, [token, childId]);
+
+	useEffect(() => {
+		setCurrRecommendations(() => {
+			const curr = recommedations?.slice(pageStart, endPage);
+			return curr;
+		});
+	}, [recommedations, pageStart, endPage]);
+
+	function handlePrev() {
+		if (pageStart === 0) return;
+		setPageStart((prev) => prev - PAGE_NUMBER);
+		setEndPage((prev) => prev - PAGE_NUMBER);
+	}
+
+	function handleNext() {
+		if (endPage < usersLength) {
+			setPageStart((prev) => prev + PAGE_NUMBER);
+			setEndPage((prev) => prev + PAGE_NUMBER);
+		}
+	}
 
 	useEffect(() => {
 		async function getChildDetails() {
@@ -74,22 +101,39 @@ const Dashboard = () => {
 								<HiOutlineAdjustmentsHorizontal className="h-8 w-8" />
 							</Link>
 						</div>
-
-						<div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-							{recommedations?.map((items) => (
-								<ProfileCard
-									state="dashboard"
-									key={items.id}
-									id={items.id}
-									age={items.age}
-									lga={items.lga}
-									firstName={items.firstName}
-									residence={items.countryofResidence}
-									about={items.about}
-									profession={items.profession}
-									gender={items.gender}
-								/>
-							))}
+						<div className="flex flex-col gap-y-8">
+							<div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+								{currRecommendations?.map((items) => (
+									<ProfileCard
+										state="dashboard"
+										key={items.id}
+										id={items.id}
+										age={items.age}
+										lga={items.lga}
+										firstName={items.firstName}
+										residence={items.countryofResidence}
+										about={items.about}
+										profession={items.profession}
+										gender={items.gender}
+									/>
+								))}
+							</div>
+							<div className="self-end flex items-center gap-x-4">
+								<button
+									onClick={handlePrev}
+									disabled={pageStart === 0}
+									className="hover:bg-[#a37eff] bg-[#BA9FFE] rounded-lg h-11 text-white font-medium box-shadow-style px-5 flex items-center gap-2"
+								>
+									Prev
+								</button>
+								<button
+									onClick={handleNext}
+									disabled={usersLength === endPage}
+									className="hover:bg-[#a37eff] bg-[#BA9FFE] rounded-lg h-11 text-white font-medium box-shadow-style px-5 flex items-center gap-2"
+								>
+									Next
+								</button>
+							</div>
 						</div>
 					</React.Fragment>
 				)}
