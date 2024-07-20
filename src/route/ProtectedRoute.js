1514d4a6cc419eb7c7b10e0b506bf0d043620165
DrPrime01 <drprime.dev@gmail.com>
2024-07-20 21:34:31 +0100

    feat: added util function decodeToken to ProtectedRoute

--- src/route/ProtectedRoute.js
+++ src/route/ProtectedRoute.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { useAuthContext } from "../context/AuthContext";
 import { Navigate, Outlet } from "react-router";
+import { decodeToken } from "../utils.js";
 
 const ProtectedRoute = () => {
 	const { user, isLoggedIn, token, logOut } = useAuthContext();
 
 	const checkTokenExpiration = () => {
 		if (token) {
-			const decodedToken = JSON.parse(atob(token.split(".")[1]));
+			const decodedToken = decodeToken(token);
 			const currentTime = Date.now() / 1000;
 
 			if (decodedToken.exp < currentTime) {
