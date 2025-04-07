53a284f06cdfcdea42a88379844107a0786ca480
dooney <delzeen55@gmail.com>
2025-04-07 15:35:34 +0100

    increased font

--- src/screens/VerifyEmailScreen.js
+++ src/screens/VerifyEmailScreen.js
@@ -8,26 +8,26 @@ const VerifyEmailScreen = () => {
 	useEffect(() => {
 		const timeoutId = setTimeout(() => {
 			navigate("/verify-otp");
-		}, 5000);
+		}, 7000);
 		return () => clearTimeout(timeoutId);
 	});
 
 	return (
-		<div className="w-100 h-screen bg-white flex flex-col items-center justify-center">
-			<div className="w-[360px] mx-auto ">
-				<img src={ClapLogo} alt="." className="w-[200px] h-[200px] mx-auto" />
-				<div className="flex flex-col items-center jutify-center mt-8">
-					<p className="text-2xl text-[#2D133A] font-medium mb-3">
-						Congratulations!!!
-					</p>
-					<p className="text-[#665e6b] text-center font-normal">
-						To get started, verify your email address by entering the otp in the
-						email we've sent you in the next page.
-					</p>
-				</div>
-			</div>
-		</div>
-	);
+    <div className="w-100 h-screen bg-white flex flex-col items-center justify-center">
+      <div className="w-[360px] mx-auto ">
+        <img src={ClapLogo} alt="." className="w-[200px] h-[200px] mx-auto" />
+        <div className="flex flex-col items-center jutify-center mt-8">
+          <p className="text-3xl text-[#2D133A] font-medium mb-3">
+            Congratulations!!!
+          </p>
+          <p className="text-[#665e6b] text-center font-semibold text-xl">
+            To get started, verify your email address by entering the{' '}
+            <bold>OTP</bold> in the email we've sent you in the next page.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default VerifyEmailScreen;
