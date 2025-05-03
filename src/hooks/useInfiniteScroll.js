a4d389a8eb37ef578e5b7bfd2a1042b13f0e8410
dooney <delzeen55@gmail.com>
2025-05-03 12:25:42 +0100

    added eslint disable

--- src/hooks/useInfiniteScroll.js
+++ src/hooks/useInfiniteScroll.js
@@ -16,35 +16,36 @@ export const useInfiniteScroll = (
     maxLimit,
     step
 ) => {
-    const pageRef = useRef(1);
-    const limitRef = useRef(baseLimit);
+  const pageRef = useRef(1);
+  const limitRef = useRef(baseLimit);
 
-    const loadMore = async () => {
-        if (!loading && hasMore) {
-            if (limitRef.current + step <= maxLimit) {
-                limitRef.current += step;
-                await fetchData(pageRef.current, limitRef.current);
-            } else {
-                pageRef.current += 1;
-                limitRef.current = baseLimit;
-                await fetchData(pageRef.current, baseLimit);
-            }
-        }
-    };
+  const loadMore = async () => {
+    if (!loading && hasMore) {
+      if (limitRef.current + step <= maxLimit) {
+        limitRef.current += step;
+        await fetchData(pageRef.current, limitRef.current);
+      } else {
+        pageRef.current += 1;
+        limitRef.current = baseLimit;
+        await fetchData(pageRef.current, baseLimit);
+      }
+    }
+  };
 
-    const handleScroll = debounce(async () => {
-        const bottomReached =
-            Math.ceil(window.innerHeight + window.scrollY) >=
-            document.documentElement.scrollHeight - 200;
+  const handleScroll = debounce(async () => {
+    const bottomReached =
+      Math.ceil(window.innerHeight + window.scrollY) >=
+      document.documentElement.scrollHeight - 200;
 
-        if (bottomReached) {
-            await loadMore();
-        }
-    }, 300);
+    if (bottomReached) {
+      await loadMore();
+    }
+  }, 300);
 
-    useEffect(() => {
-      window.addEventListener('scroll', handleScroll);
-      return () => window.removeEventListener('scroll', handleScroll);
-      // eslint-disable-next-line
-    }, []);
+  
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 };
