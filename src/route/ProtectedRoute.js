f3753806f6462f8501f7dbaeca15fc76c166d1bb
DrPrime01 <drprime.dev@gmail.com>
2025-05-28 00:14:12 +0100

    fix: redirection issue

--- src/route/ProtectedRoute.js
+++ src/route/ProtectedRoute.js
@@ -1,29 +1,34 @@
 import React from "react";
 import { useAuthContext } from "../context/AuthContext";
-import { Navigate, Outlet } from "react-router";
+import { Navigate, Outlet, useLocation } from "react-router";
 import { decodeToken } from "../utils.js";
 
 const ProtectedRoute = () => {
-	const { user, isLoggedIn, token, logOut } = useAuthContext();
+  const { user, isLoggedIn, token, logOut, authLoading } = useAuthContext();
+  const location = useLocation();
 
-	const checkTokenExpiration = () => {
-		if (token) {
-			const decodedToken = decodeToken(token);
-			const currentTime = Date.now() / 1000;
+  const checkTokenExpiration = () => {
+    if (token) {
+      const decodedToken = decodeToken(token);
+      const currentTime = Date.now() / 1000;
 
-			if (decodedToken.exp < currentTime) {
-				logOut();
-				return false;
-			}
-		}
-		return true;
-	};
+      if (decodedToken.exp < currentTime) {
+        logOut();
+        return false;
+      }
+    }
+    return true;
+  };
 
-	return user && isLoggedIn && checkTokenExpiration() ? (
-		<Outlet />
-	) : (
-		<Navigate to="/login" replace />
-	);
+  if (authLoading) return null; // or show spinner
+
+  const isValidated = Boolean(user) && isLoggedIn && checkTokenExpiration();
+
+  return isValidated ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" replace state={{ from: location }} />
+  );
 };
 
 export default ProtectedRoute;
